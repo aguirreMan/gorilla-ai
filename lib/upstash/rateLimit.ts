6266154259f02d81f7dbf807ffeb4d44eed167c1
Manuel Aguirre <aguirreman98@gmail.com>
2026-03-06 14:38:44 -0600

    Fixed the rate limiting time for upstash to 10 per hour

--- lib/upstash/rateLimit.ts
+++ lib/upstash/rateLimit.ts
@@ -4,6 +4,6 @@ import { redis } from '@/lib/upstash/redis'
 
 export const imageGenerationRateLimiting = new Ratelimit({
     redis,
-    limiter: Ratelimit.slidingWindow(5, '1 h'),
+    limiter: Ratelimit.slidingWindow(10, '1 h'),
     analytics: true
 })
