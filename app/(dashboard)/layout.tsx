d3dc4b87499a37ca227fa52558c9ac23e7a2e5d2
Manuel Aguirre <aguirreman98@gmail.com>
2026-05-03 00:25:33 -0500

    REfactored my layout in the dashboard just took out tanstack

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,6 @@
 import Dashboardnav from '@/components/dashboard-components/Dashboardnav'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'sonner'
-import ReactQueryProvider from '@/providers/QueryProvider'
 
 export default function Dashboardlayout({
   children,
@@ -12,22 +11,20 @@ export default function Dashboardlayout({
 }) {
   return (
     <ClerkProvider>
-      <ReactQueryProvider>
-        <Toaster
-          position='top-center'
-          toastOptions={{
-            duration: 4000,
-          }}
-          richColors
-          closeButton
-        />
-          <div className='flex flex-col min-h-screen bg-background'>
-            <Dashboardnav />
-            <main className='flex-1 pt-16'>
-              {children}
-            </main>
-          </div>
-      </ReactQueryProvider>
+      <Toaster
+        position='top-center'
+        toastOptions={{
+        duration: 4000,
+      }}
+        richColors
+        closeButton
+      />
+      <div className='flex flex-col min-h-screen bg-background'>
+        <Dashboardnav />
+          <main className='flex-1 pt-16'>
+            {children}
+          </main>
+      </div>
     </ClerkProvider>
   )
 }
